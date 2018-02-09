3322fbcafa689084f231191d97d97fdd3a279198
Bjarkihv <Bjarkiv93@gmail.com>
2018-02-09 15:38:36 +0000

    Login og register, fyrsta commit

---
+++ AndroidStudioProjects/Skater/app/src/main/java/isbhv2/hi/notandi/skater/LoginActivity.java
@@ -0,0 +1,13 @@
+package isbhv2.hi.notandi.skater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
