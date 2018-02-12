1eeaea020fb3eac53c5373a439d04cc40d82bbea
ggs12 <gretarsaemundsson@gmail.com>
2018-02-12 12:41:36 +0000

    AddSpot og Findspot bætt við , eru tómir samt.  Er aðallega að athuga hvort ég næ að committa

---
+++ AndroidStudioProjects/Skater/app/src/main/java/isbhv2/hi/notandi/skater/AddSpotActivity.java
@@ -0,0 +1,13 @@
+package isbhv2.hi.notandi.skater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSpotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_spot);
+    }
+}
