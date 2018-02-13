b04473c08d9b2d35a983923ee981ad9a298fb40a
ggs12 <gretarsaemundsson@gmail.com>
2018-02-13 21:39:27 +0000

    Hrátt grunnviðmót fyrir findSpot og fleira

--- AndroidStudioProjects/Skater/app/src/main/java/isbhv2/hi/notandi/skater/FindSpotActivity.java
+++ AndroidStudioProjects/Skater/app/src/main/java/isbhv2/hi/notandi/skater/FindSpotActivity.java
@@ -2,6 +2,7 @@ package isbhv2.hi.notandi.skater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class FindSpotActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class FindSpotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_spot);
+
+        final EditText searchInputString = (EditText) findViewById(R.id.searchInput);
     }
+
+
 }
--- AndroidStudioProjects/Skater/app/src/main/java/isbhv2/hi/notandi/skater/UserAreaActivity.java
+++ AndroidStudioProjects/Skater/app/src/main/java/isbhv2/hi/notandi/skater/UserAreaActivity.java
@@ -4,15 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class UserAreaActivity extends AppCompatActivity {
 
+    private Button findSpotLaunch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+        findSpotLaunch = (Button) findViewById(R.id.button2);
+
+        findSpotLaunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
 
         final EditText nsUsername = (EditText) findViewById(R.id.nsUsername);
         final TextView nsWelcome = (TextView) findViewById(R.id.nsLabel);
+    }
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, FindSpotActivity.class);
+        startActivity(intent);
     }
 }
