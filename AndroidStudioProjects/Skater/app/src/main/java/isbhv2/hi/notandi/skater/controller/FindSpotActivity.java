847a99c1a69cb1610eaab827091f298cfa0bc181
Bjarkihv <Bjarkiv93@gmail.com>
2018-02-14 21:09:09 +0000

    Fix

--- AndroidStudioProjects/Skater/app/src/main/java/isbhv2/hi/notandi/skater/controller/FindSpotActivity.java
+++ AndroidStudioProjects/Skater/app/src/main/java/isbhv2/hi/notandi/skater/controller/FindSpotActivity.java
@@ -1,9 +1,9 @@
 package isbhv2.hi.notandi.skater.controller;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
